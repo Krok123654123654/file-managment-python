66c10b0255328ed7b3b0f9cefd42d2b7710eb7fe
Sender012 <js459491@students.mimuw.edu.pl>
2024-11-04 22:40:33 +0100

    Added reading paths

--- main.py
+++ main.py
@@ -53,37 +53,44 @@ def odczyt_z_csv(catalog):
    print("Suma Czasu dla danego katalogu to:" + str(sum) + "s")
 
 def get_time(i,times):
-   if (i > len(times)):
+   if (i < len(times)):
       return times_full.get(times[i])
    return times_full.get("r")
 
-def generate_paths(args):
+def generate_or_read_paths(args):
    paths = []
    days_index = 0
    for months, days in zip(args.months, args.days) :
       days_index = 0
-      index1,index2
+      index1,index2 = 0, 0
       if("-" in days):
          parts = days.split("-")
-         
+
          if(len(parts) > 2):
             raise ValueError("Zła składnia dni")
-         
-         index1 = days_full.index(parts[0])
-         index2 = days_full.index(parts[1]) + 1
+
+         index1 = days_order.index(parts[0])
+         index2 = days_order.index(parts[1]) + 1
       else :
-         index1 = days_full.index(days)
+         index1 = days_order.index(days)
          index2 = index1 + 1
-      
       for i in list(days_full.keys())[index1 : index2]:
-            if(args.read_or_write == "t"):
-               paths.append(os.path.join(months,i,get_time(days_index)))
+            if(args.read_or_write !="t"):
+               paths.append(os.path.join(os.getcwd(),months,days_full[i],get_time(days_index, args.times)))
             else :
-               paths.append(os.path.join(months,i,get_time(days_index),"plik.csv"))
-      
-      for path in paths :
+               paths.append(os.path.join(os.getcwd(),months,days_full[i],get_time(days_index, args.times),"Dane.csv"))
+            days_index+=1
+   print(paths)
+   if(args.read_or_write != "t"):
+      for path in paths:
+         odczyt_z_csv(path)
+   else:
+      for path in paths:
          os.makedirs(path, exist_ok=True)
-      
+         zapis_do_csv(path)
+
+
+
 def main():
    parser = argparse.ArgumentParser()
    parser.add_argument("--months", type=str, nargs='+', help="wybor miesiecy")
@@ -99,5 +106,3 @@ def main():
 
    if(len(args.miesiace) != len(args.dni)):
       raise ValueError("Dla każdego miesiąca musi być podany dzień")
-   
-## Krokson jest smrodem
