a8df6107a7a9e80df045b1ce95b00ca69f2a3e69
Sender012 <js459491@students.mimuw.edu.pl>
2024-11-04 22:04:39 +0100

    przepraszam za poprzedni komentarz

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 import argparse
 import csv
 import random
+import os
+
+
+# Mappings for days and times
+days_order = ['pn', 'wt', 'śr', 'cz', 'pt', 'sb', 'nd']
+days_full = {
+   'pn': 'poniedziałek',
+   'wt': 'wtorek',
+   'śr': 'środa',
+   'cz': 'czwartek',
+   'pt': 'piątek',
+   'sb': 'sobota',
+   'nd': 'niedziela'
+}
+
+times_full = {
+   'r': 'rano',
+   'w': 'wieczorem'
+}
 
 def zapis_do_csv(filepath):
-   """ Tworzy plik `Dane.csv` składający się z dokładnie dwóch linii: 
-   Model; Wynik; Czas; 
-   A ; 17 ; 465s; Wartości w drugiej lini są dobierane losowo """
+   ## Tworzy plik `Dane.csv` składający się z dokładnie dwóch linii: 
+   ##Model; Wynik; Czas; 
+   ##A ; 17 ; 465s; Wartości w drugiej lini są dobierane losowo ##
    headers = ["Model", "Wynik", "Czas"]
    model = random.choice(["A", "B", "C"])
    wynik = random.randint(0, 1000)
@@ -18,31 +37,62 @@ def zapis_do_csv(filepath):
    print("Utworzono Plik Dane.csv")
 
 def odczyt_z_csv(catalog):
-   """ Sumuje wartości z kolumny `Czas` dla wszystkich plików CSV w katalogu,
-   gdzie `Model` wynosi 'A'. """
+   ## Sumuje wartości z kolumny `Czas` dla wszystkich plików CSV w katalogu,
+   ##gdzie `Model` wynosi 'A'. ##
    sum = 0
-   for elder, _, files in os.walk(catalog): """ Przechodząc po wszystkich plikach danego katalogu"""
+   for elder, _, files in os.walk(catalog): ## Przechodząc po wszystkich plikach danego katalogu##
       for file in files:
-         if file.endswith('.csv'):   """ Wybieramy tylko te które mają rozszerzenie csv"""
+         if file.endswith('.csv'):   ## Wybieramy tylko te które mają rozszerzenie csv##
             filepath = os.path.join(elder, file)
-            with open(filepath, mode='r') as file:   """Odczytujemy plik"""
+            with open(filepath, mode='r') as file:   ##Odczytujemy plik##
                reader = csv.DictReader(file, delimiter=';')
                for row in reader:
-                   if row['Model'] == 'A':"""Jesli Model to A, wtedy dodajemu czas (najpierw usuwamy jednostke i zmieniamy na int)"""
+                   if row['Model'] == 'A':##Jesli Model to A, wtedy dodajemu czas (najpierw usuwamy jednostke i zmieniamy na int)##
                       time_val = int(row['Czas'].replace('s', ''))
                       sum += time_val
    print("Suma Czasu dla danego katalogu to:" + str(sum) + "s")
-   
 
+def get_time(i,times):
+   if (i > len(times)):
+      return times_full.get(times[i])
+   return times_full.get("r")
+
+def generate_paths(args):
+   paths = []
+   days_index = 0
+   for months, days in zip(args.months, args.days) :
+      days_index = 0
+      index1,index2
+      if("-" in days):
+         parts = days.split("-")
+         
+         if(len(parts) > 2):
+            raise ValueError("Zła składnia dni")
+         
+         index1 = days_full.index(parts[0])
+         index2 = days_full.index(parts[1]) + 1
+      else :
+         index1 = days_full.index(days)
+         index2 = index1 + 1
+      
+      for i in list(days_full.keys())[index1 : index2]:
+            if(args.read_or_write == "t"):
+               paths.append(os.path.join(months,i,get_time(days_index)))
+            else :
+               paths.append(os.path.join(months,i,get_time(days_index),"plik.csv"))
+      
+      for path in paths :
+         os.makedirs(path, exist_ok=True)
+      
 def main():
    parser = argparse.ArgumentParser()
-   parser.add_argument("--miesiace", type=str, nargs='+', help="wybor miesiecy")
-   parser.add_argument("--dni", type=str, nargs='+', help="wybor dni")
+   parser.add_argument("--months", type=str, nargs='+', help="wybor miesiecy")
+   parser.add_argument("--days", type=str, nargs='+', help="wybor dni")
    parser.add_argument(
-      "--pora_dnia", type=str, default="r", nargs="+", required = False, help="Jaka pora dnia (r/w), domyślnie rano"
+      "--times", type=str, default="r", nargs="+", required = False, help="Jaka pora dnia (r/w), domyślnie rano"
    )
    parser.add_argument(
-      "--tworzenie_odczyt", type=str, default="t", nargs="+", required = False, help="Tworzenie lub odczyt(t/o), domyślnie odczyt"
+      "--read_or_write", type=str, default="t", nargs="+", required = False, help="Tworzenie lub odczyt(t/o), domyślnie odczyt"
    )
    args = parser.parse_args()
 
@@ -51,5 +101,3 @@ def main():
       raise ValueError("Dla każdego miesiąca musi być podany dzień")
    
 ## Krokson jest smrodem
-
-
